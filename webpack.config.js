58df98d770c4d50e28054b902daca495bd43c2d5
Guillaume Chau <alphadelta.fg@gmail.com>
2017-07-05 15:23:06 +0200

    Bundle size optimizations

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,15 @@
 var path = require('path')
 var webpack = require('webpack')
+var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+
+var momentLocales = /en/
 
 module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
-    filename: 'build.js'
+    filename: 'build.js',
   },
   module: {
     rules: [
@@ -19,22 +22,22 @@ module.exports = {
             // the "scss" and "sass" values for the lang attribute to the right configs here.
             // other preprocessors should work out of the box, no loader config like this nessessary.
             'scss': 'vue-style-loader!css-loader!sass-loader',
-            'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
-          }
+            'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax',
+          },
           // other vue-loader options go here
-        }
+        },
       },
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
         loader: 'file-loader',
         options: {
-          name: '[name].[ext]?[hash]'
-        }
+          name: '[name].[ext]?[hash]',
+        },
       },
       {
         test: /\.css$/,
@@ -45,31 +48,32 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'graphql-tag/loader',
       },
-    ]
+    ],
   },
   resolve: {
     modules: ['src', 'node_modules'],
-    extensions: ['.js', '.json', '.vue']
+    extensions: ['.js', '.json', '.vue'],
     /* alias: {
       'vue$': 'vue/dist/vue.common.js'
     } */
   },
   devServer: {
     historyApiFallback: true,
-    noInfo: true
+    noInfo: true,
   },
   performance: {
-    hints: false
+    hints: false,
   },
   devtool: '#eval-source-map',
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify(process.env.NODE_ENV),
-        GRAPHQL_URL: JSON.stringify(process.env.GRAPHQL_URL || 'https://vue-curated-api.now.sh/graphql')
-      }
-    })
-  ]
+        GRAPHQL_URL: JSON.stringify(process.env.GRAPHQL_URL || 'https://vue-curated-api.now.sh/graphql'),
+      },
+    }),
+    new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, momentLocales),
+  ],
 }
 
 if (process.env.NODE_ENV === 'production') {
@@ -79,11 +83,17 @@ if (process.env.NODE_ENV === 'production') {
     new webpack.optimize.UglifyJsPlugin({
       sourceMap: true,
       compress: {
-        warnings: false
-      }
+        warnings: false,
+      },
     }),
     new webpack.LoaderOptionsPlugin({
-      minimize: true
-    })
+      minimize: true,
+    }),
+  ])
+}
+
+if (process.env.ANALYZE === 'on') {
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new BundleAnalyzerPlugin(),
   ])
 }
